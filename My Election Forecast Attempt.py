e3babaf37443935dcdb8b93b3e7892e0a2946edc
ddmayes <69473067+ddmayes@users.noreply.github.com>
2020-11-17 15:58:28 -0500

    Update My Election Forecast Attempt.py

--- My Election Forecast Attempt.py
+++ My Election Forecast Attempt.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import csv 
 from sklearn import preprocessing
 
 
-# In[3]:
 
 
 #Read in the data as a pd dataframe
@@ -17,7 +10,6 @@ data_2016 = pd.read_csv('presidential_polls_2016.csv')
 data_2016.head()
 
 
-# In[4]:
 
 
 #Reorganize data
@@ -28,7 +20,6 @@ data_2016["enddate"] = pd.to_datetime(data_2016["enddate"])
 data_2016.head()
 
 
-# In[5]:
 
 
 #Creation of the democratic "edge" column
@@ -36,7 +27,6 @@ data_2016["dem_edge"] = data_2016['clinton'] - data_2016['trump']
 data_2016.head()
 
 
-# In[2]:
 
 
 #Now a similar dataset for 2020
@@ -44,7 +34,6 @@ data_2020 = pd.read_csv('2020 US presidential election polls - all_polls.csv')
 data_2020.head()
 
 
-# In[3]:
 
 
 #Drop unecessary cols and reorganize
@@ -55,13 +44,12 @@ data_2020["dem_edge"] = data_2020["biden"] - data_2020["trump"]
 data_2020.head()
 
 
-# In[4]:
 
 
+#Again creating the edge value column
 data_2020['dem_edge'] = data_2020['dem_edge'] / 100.0
 
 
-# In[8]:
 
 
 #Create unique set of states and pollsters
@@ -69,6 +57,9 @@ states = set()
 for val in data_2016['state']:
     states.add(val)
 
+#For simplicity, I've dropped congressional districts of Nebraska and Maine, data provided also did not
+#include Alaska or DC so this will result in a total elector count 15 short of the actual 538
+#but it's worth noting that this is a rough approximation model
 pollsters = set()
 for val in data_2016['pollster']:
     pollsters.add(val)
@@ -82,15 +73,13 @@ states.remove('U.S.')
 states.remove('Alaska')
 
 
-# In[9]:
 
 
-#The validation data
+#The validation data/actual outcome in each state for the 2016 election
 results_2016 = pd.read_csv('nytimes_presidential_elections_2016_results_county.csv')
 results_2016.head()
 
 
-# In[10]:
 
 
 #The actual outcome in each state. If positive, democratic, assuming winner take all
@@ -118,14 +107,12 @@ for val in actual_results:
 print(dem_or_rep_victory_by_state)
 
 
-# In[11]:
 
 
 data_2016['dem_edge'] = data_2016['dem_edge'] / 100.0
 print(data_2016['dem_edge'])
 
 
-# In[16]:
 
 
 the_total = 0
@@ -141,7 +128,6 @@ actual_results['Nebraska CD-3'] = the_total / len(actual_results)
 actual_results['District of Columbia'] = the_total / len(actual_results)
 
 
-# In[21]:
 
 
 data_2016['actual_outcome']
@@ -152,7 +138,8 @@ for row in data_2016['state']:
 data_2016.head()
 
 
-# In[24]:
+#Now for a simple prediction model using solely
+#poll leads as the features
 
 
 from sklearn.ensemble import RandomForestRegressor
@@ -172,7 +159,6 @@ error = mean_absolute_error(model_predictions, valid_y)
 print("Mean absolute error for this model: ", error)
 
 
-# In[5]:
 
 
 #Onto 2020!
@@ -180,16 +166,14 @@ electoral = pd.read_csv('electoral-college.csv')
 electoral.head()
 
 
-# In[6]:
 
-
+#Organizing elector columns by state
 electors = {}
 for i in range(len(electoral)):
     electors[electoral['State'][i]] = electoral['Electors'][i]
     i = i + 1
 
 
-# In[41]:
 
 
 data_2020['electors'] = '0'
@@ -207,7 +191,6 @@ for row in data_2020['state']:
 data_2020.head()
 
 
-# In[42]:
 
 
 #Abbreviated states
@@ -217,7 +200,6 @@ for state in data_2020['state']:
 print(ab_states)    
 
 
-# In[43]:
 
 
 trump_elector = 0
@@ -233,7 +215,6 @@ for state in ab_states:
 print(average_edge_by_state)
 
 
-# In[61]:
 
 
 for state in ab_states:
@@ -244,72 +225,6 @@ for state in ab_states:
 
 print("Trump's predicted elector count: ", trump_electors)
 print("Biden's predicted elector count: ", biden_electors)
-
-
-# In[16]:
-
-
-better_data_2020 = pd.read_csv('president_polls.csv')
-better_data_2020.head()
-
-
-# In[17]:
-
-
-better_data_2020 = better_data_2020[["start_date", "end_date", "state", "fte_grade", "pollster", "answer", "pct"]]
-better_data_2020.head()
-
-
-# In[18]:
-
-
-new2020data = better_data_2020[(better_data_2020["answer"] == "Biden") | (better_data_2020["answer"] == "Trump")]
-new2020data = new2020data.pivot_table(values = 'pct', index=['start_date', 'end_date', 'state', 'fte_grade', 'pollster'], columns='answer')
-new2020data = new2020data.dropna(axis = 0, how = 'any')
-new2020data.head()
-
-
-# In[19]:
-
-
-new2020data['dem_edge'] = (new2020data["Biden"] - new2020data["Trump"]) / 100.0
-new2020data.head()
-
-
-# In[20]:
-
-
-new2020data['electors'] = '0'
-k = 0
-for row in new2020data['state']:
-    if row != '--':
-        new2020data['electors'][k] = electors[row]
-        k = k + 1
-    elif row == '--':
-        data_2020['electors'][k] = 0
-        k = k + 1
-    else:
-        k = k + 1
-        continue
-new2020data.head()
-
-
-# In[25]:
-
-
-states_2020_pt2 = set()
-for state in new2020data["state"]:
-    states_2020_pt2.add(state)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
